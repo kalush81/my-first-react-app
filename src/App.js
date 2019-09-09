80b53e0b3897ab6ae0aa249c68d1d8d488b24f3b
kalush81 <krzysiek.kaluza@gmail.com>
2019-09-09 13:23:09 +0200

    change greeting

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to Codaisseur
+          Welcome to Codaisseur, hello ...
         </a>
       </header>
     </div>
